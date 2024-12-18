f8569557a41af21e39a869b90e79321ead3a80e3
atul <atulware.22@gmail.com>
2024-12-18 20:45:45 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("this is my response")
+})
+
+app.get("/test",(req,res)=>{
+    res.send("this is my response for test")
+})
+
+app.listen(7777,()=>{
+    console.log('your server is successfully establish on port 7777')
+})
